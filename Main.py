15972d0a063bc14f5b521f3c4b6662e3a60d36fc
Hugo.Duarte <hugo.s.duarte@icloud.com>
2023-01-19 22:53:04 -0300

    Include more buttons to check

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 # LiTeMacro V2.0
 # Done By Hugo Duarte
 
-#Requirements: A HQ icon image of misson complete scroll, send on quest blue button and confirm green button.
+# Requirements: A HQ icon image of misson complete scroll, send on quest blue button and confirm green button.
 
 import pyautogui
 import time
@@ -9,54 +9,95 @@ import psutil
 from screeninfo import get_monitors
 
 
+def check_mission():
+    # Esperar a conclusão da missão
+    print('Iniciando a validação/envio do char')
+    image_name = resolution+'QuestCompleted.png'
+    icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+
+    x = 0
+
+    while icon is None:
+        print("Não foi possivel encontrar o icone de missão concluida, refazendo análise.")
+        time.sleep(1)
+        icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+        if x == 6:
+            print('Desencontro das etapas. voltando ao menu inicial..')
+            pyautogui.press('esc')
+            main_menu = resolution+'Back.png'
+            time.sleep(2)
+            icon_menu = pyautogui.locateCenterOnScreen(main_menu, confidence=nr_confidence)
+            time.sleep(2)
+            if icon_menu is not None:
+                pyautogui.click(icon_menu[0], icon_menu[1])
+            x = 0
+
+        x = x+1
+
+    print('Icone encontrado, iniciando o procedimento de reenvio')
+    time.sleep(2)
+    pyautogui.click(icon[0], icon[1])
+
+
 def start_quest():
-    time.sleep(1)
-    
     # Solicitar envio para missão
     print('Buscando botão para envio da missão')
-    image_name = 'SendOnQuest.png'
-    icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
+    image_name = resolution+'SendOnQuest.png'
+    icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+
+    x = 0
 
     while icon is None:
-        print("Não foi possivel encontrar o icone de envio, refazendo análise.") 
+        print("Não foi possivel encontrar o icone de envio, refazendo análise.")
         time.sleep(1)
-        icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
+        icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+        if x == 6:
+            break
+        x = x+1
+
+    if x == 6:
+        check_mission()
 
     print('Icone encontrado, iniciando o procedimento de envio')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.click(icon[0], icon[1])
 
+
+def confirm_quest():
     # Confirmando envio para missão
     print('Buscando botão de confirmação da missão')
-    image_name = 'Confirm.png'
-    icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
+    image_name = resolution+'Confirm.png'
+    icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+
+    x = 0
 
     while icon is None:
-        print("Não foi possivel encontrar o icone de confirmação, refazendo análise.") 
+        print("Não foi possivel encontrar o icone de confirmação, refazendo análise.")
         time.sleep(1)
-        icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
+        icon = pyautogui.locateCenterOnScreen(image_name, confidence=nr_confidence)
+        if x == 6:
+            break
+        x = x+1
+
+    if x == 6:
+        start_quest()
 
     print('Icone encontrado, confirmando o envio para missão')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.click(icon[0], icon[1])
-
-    return True
 
 
 print('Iniciando macro..')
+time.sleep(2)
+for m in get_monitors():
+    resolution = str(m.width)+'x'+str(m.height)
+
+if resolution == '1920x1080':
+    nr_confidence = 0.8
+else:
+    nr_confidence = 0.5
 
 while "LiteBringer.exe" in (i.name() for i in psutil.process_iter()):
-    # Esperar a conclusão da missão
-    print('Iniciando a validação/envio do char')
-    image_name = 'QuestCompleted.png'
-    icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
-
-    while icon is None:
-        print("Não foi possivel encontrar o icone de missão concluida, refazendo análise.") 
-        time.sleep(1)
-        icon = pyautogui.locateCenterOnScreen(image_name, confidence=0.8)
-
-    print('Icone encontrado, iniciando o procedimento de reenvio')
-    time.sleep(1)
-    pyautogui.click(icon[0], icon[1])
+    check_mission()
     start_quest()
+    confirm_quest()
